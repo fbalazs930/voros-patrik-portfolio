3fb045e2caee311538bdba7ef0db9b85a0f38e77
fbalazs930 <fbalazs930@gmail.com>
2021-08-17 11:58:01 +0200

    db src change

--- src/components/Gallery/Events.jsx
+++ src/components/Gallery/Events.jsx
@@ -1,49 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from 'react';
-import Images from './Images.jsx';
+import React from 'react';
+import ImagesDB from './Images.jsx';
 
-export const Events = () => {
-    const [id, setId] = useState(0);
-    const [click, setClick] = useState(false);
-    const exit = () => {
-        setClick(false);
-    }
-
-    const updateId = e => {
-        if (e.keyCode === 27) { exit() }
-        if (e.keyCode === 37) { if (id - 1 >= 0) { setId(id - 1) } }
-        if (e.keyCode === 39) {
-            if (id + 1 <= Images.events.length - 1) {
-                setId(id + 1);
-            }
-        }
-    }
-    useEffect(() => {
-        window.addEventListener('keyup', updateId);
-        return () => { window.removeEventListener('keyup', updateId); }
-    }, [id])
+export const Events = () => {    
+    const images=ImagesDB.events;
     return (
         <div className='events'>
             <h1>Rendezvényfotók</h1>
             <div className="images">
-                {Images.events.map(img => (
-                    <img key={img.id} src={img.src} alt="Étel" onClick={() => { setId(img.id); setClick(true) }}   />
+                {images.map(img => (
+                    <img key={img.id} src={img.src} alt="Étel"/>
                 ))}
             </div>
-            {click &&
-                <div className="full-image">
-                    <img src={Images.events[id].src} alt="" />
-                    <i className="fas fa-arrow-circle-left" onClick={() => {
-                        if (id - 1 >= 0) { setId(id - 1) }
-                    }}>
-                    </i>
-                    <i className="fas fa-arrow-circle-right" onClick={() => {
-                        if (id + 1 <= Images.events.length - 1) { setId(id + 1) }
-                    }}>
-                    </i>
-                    <i className="far fa-times-circle" onClick={() => exit()}></i>
-                </div>
-            }
         </div>
     )
 }
--- src/components/Gallery/Portraits.jsx
+++ src/components/Gallery/Portraits.jsx
@@ -1,21 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from 'react';
-import Images from './Images.jsx';
+import React, { useState } from 'react';
+import ImagesDB from './Images.jsx';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
 export const Portraits = () => {
-
+    const images=ImagesDB.portraits;
+    const [click, setClick] = useState(false);
     return (
         <div className='portraits'>
             <h1>Portréfotók</h1>
-            <Carousel className="carousel" emulateTouch autoPlay interval="4000" showThumbs="false" useKeyboardArrows stopOnHover>
-                {Images.portraits.map(img => (
-                    <div key={img.id}>
-                        <img src={img.src} alt="portré" />
-                    </div>
+            <div className="images">
+                {images.map(img => (
+                    <img key={img.id} src={img.src} alt="Étel" />
                 ))}
-            </Carousel>
+            </div>
+
+            {click === true &&
+                <Carousel className="carousel" emulateTouch autoPlay interval="4000" showThumbs="false" useKeyboardArrows stopOnHover>
+                    {images.map(img => (
+                        <div key={img.id}>
+                            <img src={img.src} alt="portré" />
+                        </div>
+                    ))}
+                </Carousel>
+            }
         </div>
     )
 }
