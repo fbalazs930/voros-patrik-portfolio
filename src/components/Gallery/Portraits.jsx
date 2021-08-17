490894b796a42950e7f8cd7b82c493c00fb218ff
fbalazs930 <fbalazs930@gmail.com>
2021-08-17 11:46:53 +0200

    gallery

--- src/components/Gallery/Portraits.jsx
+++ src/components/Gallery/Portraits.jsx
@@ -1,52 +1,21 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Images from './Images.jsx';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 export const Portraits = () => {
-    const [id, setId] = useState(0);
-    const [click, setClick] = useState(false);
-    const exit = () => {
-        setClick(false);
-    }
 
-
-    const updateId = e => {
-        if (e.keyCode === 27) { exit() }
-        if (e.keyCode === 37) { if (id - 1 >= 0) { 
-            setId(id - 1) ;
-        } }
-        if (e.keyCode === 39) {
-            if (id + 1 <= Images.portraits.length - 1) {
-                setId(id + 1);
-            }
-        }
-    }
-    useEffect(() => {
-        window.addEventListener('keyup', updateId);
-        return () => { window.removeEventListener('keyup', updateId); }
-    }, [id])
     return (
         <div className='portraits'>
             <h1>Portréfotók</h1>
-            <div className="images">
+            <Carousel className="carousel" emulateTouch autoPlay interval="4000" showThumbs="false" useKeyboardArrows stopOnHover>
                 {Images.portraits.map(img => (
-                    <img key={img.id} src={img.src} alt="Étel" onClick={() => { setId(img.id); setClick(true) }}  />
+                    <div key={img.id}>
+                        <img src={img.src} alt="portré" />
+                    </div>
                 ))}
-            </div>
-            {click &&
-                <div className="full-image">
-                    <img src={Images.portraits[id].src} alt="" />
-                    <i className="fas fa-arrow-circle-left" onClick={() => {
-                        if (id - 1 >= 0) { setId(id - 1) }
-                    }}>
-                    </i>
-                    <i className="fas fa-arrow-circle-right" onClick={() => {
-                        if (id + 1 <= Images.portraits.length - 1) { setId(id + 1) }
-                    }}>
-                    </i>
-                    <i className="far fa-times-circle" onClick={() => exit()}></i>
-                </div>
-            }
+            </Carousel>
         </div>
     )
 }
