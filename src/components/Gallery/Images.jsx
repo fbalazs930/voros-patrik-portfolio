483aa6019762a99cd9a72942f06135adffb5ec88
fbalazs930 <fbalazs930@gmail.com>
2021-08-25 13:14:40 +0200

    more img

--- src/components/Gallery/Images.jsx
+++ src/components/Gallery/Images.jsx
@@ -22,7 +22,27 @@ const images = {
         },
         {
             id: 5,
+            src: 'https://i.postimg.cc/Y95CGwVg/IMG-39450806.jpg'
+        },
+        {
+            id: 6,
             src: 'https://i.postimg.cc/Qx6v0q8Y/IMG-40442106.jpg'
+        },
+        {
+            id: 7,
+            src: 'https://i.postimg.cc/xdFcfxzz/IMG-39822106.jpg'
+        },
+        {
+            id: 8,
+            src: 'https://i.postimg.cc/vHLTgHnM/IMG-40312106.jpg'
+        },
+        {
+            id: 9,
+            src: 'https://i.postimg.cc/nL5hbXyK/IMG-42332106.jpg'
+        },
+        {
+            id: 10,
+            src: 'https://i.postimg.cc/8krQnKk0/260.jpg'
         }
     ],
     dishes: [
@@ -54,17 +74,57 @@ const images = {
         },
         {
             id: 1,
-            src: 'https://i.postimg.cc/VsZ3FRLG/194.png'
+            src: 'https://i.postimg.cc/BvGXXLYq/194.jpg'
+        },
+        {
+            id: 2,
+            src: 'https://i.postimg.cc/Vv4jF9hB/174.jpg'
+        },
+        {
+            id: 3,
+            src: 'https://i.postimg.cc/2yqzHrcc/IMG-42852606.jpg'
+        },
+        {
+            id: 4,
+            src: 'https://i.postimg.cc/d0qwb8NT/IMG-42862606.jpg'
+        },
+        {
+            id: 5,
+            src: 'https://i.postimg.cc/9Q2DfwNc/IMG-42872606.jpg'
+        },
+        {
+            id: 6,
+            src: 'https://i.postimg.cc/q7hHVjXq/kupikupa1.jpg'
+        },
+        {
+            id: 7,
+            src: 'https://i.postimg.cc/Nfshfg7W/kupikupa3.jpg'
         }
     ],
     lansdscapes: [
         {
             id: 0,
-            src: 'https://i.postimg.cc/s2Pk9Cv7/215.jpg'
+            src: 'https://i.postimg.cc/Y2xVP5K8/IMG-40522106.jpg'
         },
         {
             id: 1,
-            src: 'https://i.postimg.cc/VsZ3FRLG/194.png'
+            src: 'https://i.postimg.cc/QdFyHVBn/IMG-40572106.jpg'
+        },
+        {
+            id: 2,
+            src: 'https://i.postimg.cc/br6mvjqh/IMG-40602106.jpg'
+        },
+        {
+            id: 3,
+            src: 'https://i.postimg.cc/pr1BHYBV/IMG-40812106.jpg'
+        },
+        {
+            id: 4,
+            src: 'https://i.postimg.cc/qMzLGSkN/IMG-40972106.jpg'
+        },
+        {
+            id: 5,
+            src: 'https://i.postimg.cc/mrNNDGS5/IMG-41672106.jpg'
         }
     ]
 }
