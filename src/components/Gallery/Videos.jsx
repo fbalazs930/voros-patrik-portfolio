c1dc3fd28dde63abab8d0c54b6e31c9d9d1d102c
fbalazs930 <fbalazs930@gmail.com>
2021-11-08 19:43:55 +0100

    resp

--- src/components/Gallery/Videos.jsx
+++ src/components/Gallery/Videos.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-
+import React from "react";
 
 export const Videos = () => {
-    
-    return (
-        <div className='videos p'>
-            <h1>Videók</h1>
-                <div className="video">
-                    <iframe
-                    src={`https://www.youtube.com/embed/P-L-grmh8-U`}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    title="Embedded youtube"
-                    />
-                </div>
+  return (
+    <div className="videos p">
+      <h1>Videók</h1>
+      <div class="video-wrap">
+        <div className="video-container">
+          <iframe
+            title="bme-napok"
+            src="https://www.youtube.com/embed/P-L-grmh8-U"
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,47 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-    const closeMenu = () => { setClick(false) }; 
-    window.addEventListener('resize', () => {
-        if (window.innerWidth < 830) {
-            setClick(false);
-        }
-    });
-    return (
-        <div className='nav'>
-            <i className="fas.fa-bars"></i>
-            <img src="https://i.postimg.cc/g0NZ5nqV/VP-logos-white.png" alt="logo" className="logo" />
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                <Link onClick={closeMenu} to='/' className="nav-link">
-                    Rólam
-                </Link>
-                <Link onClick={closeMenu} to='/fotok' className="nav-link">
-                    Fotók
-                </Link>
-                <Link onClick={closeMenu} to='/videok' className="nav-link">
-                    Videók
-                </Link>
-                <Link onClick={closeMenu} to='/elerhetoseg' className="nav-link">
-                    Elérhetőség
-                </Link>
-            </ul>
-            <div className="menu-icon" onClick={handleClick}>
-                <i className={click ? 'far fa-times-circle' : 'fas fa-bars'} />
-            </div>
-        </div>
-    )
-}
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => {
+    setClick(false);
+  };
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 830) {
+      setClick(false);
+    }
+  });
+  return (
+    <div className="nav">
+      <i className="fas.fa-bars"></i>
+      <Link to="/" className="nav-link">
+        <img
+          src="https://i.postimg.cc/g0NZ5nqV/VP-logos-white.png"
+          alt="logo"
+          className="logo"
+        />
+      </Link>
+
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <Link onClick={closeMenu} to="/" className="nav-link">
+          Rólam
+        </Link>
+        <Link onClick={closeMenu} to="/fotok" className="nav-link">
+          Fotók
+        </Link>
+        <Link onClick={closeMenu} to="/videok" className="nav-link">
+          Videók
+        </Link>
+        <Link onClick={closeMenu} to="/elerhetoseg" className="nav-link">
+          Elérhetőség
+        </Link>
+      </ul>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={click ? "far fa-times-circle" : "fas fa-bars"} />
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
